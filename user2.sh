4b031cf23e9fd925289cbfb22fb20d961c48dce0
root <root@ip-172-31-1-113.ap-south-1.compute.internal>
2019-12-03 07:44:23 +0000

    Second Commit: Updated EXITStatus

--- user2.sh
+++ user2.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+USER_NAME=$(id -un)
+USER_ID=$UID
+
+#Display UID
+echo "The user ID = $UID"
+#If condition to EXIT ouput if the condition not equals to user id 1000 or else.
+if [[ 1000 -le $USER_ID ]]
+then
+	echo " $USER_NAME 's user ID is $UID "
+	exit 1
+fi
+
 
