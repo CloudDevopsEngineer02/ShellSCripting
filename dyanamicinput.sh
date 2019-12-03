357ed89eb0f08b54eccd00898ec3fa181abcb289
root <root@ip-172-31-1-113.ap-south-1.compute.internal>
2019-12-03 11:19:56 +0000

    Dynamic inputs of users list

---
+++ dyanamicinput.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Enter the Name of the employee: " EMP
+
+read -s -p " Enter the Password of the employee:" PASSWORD
+
+
+echo "Employee Name is:  ${EMP} and Password: $PASSWORD "
+
+
+
